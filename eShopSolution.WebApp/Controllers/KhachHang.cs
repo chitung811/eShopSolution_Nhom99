1110b9163945d18bd9a4fb206b34797280482398
Truong Phuong <truongphuongvt1107@gmail.com>
2021-01-04 19:53:21 +0700

    thêm template

---
+++ eShopSolution.WebApp/Controllers/KhachHang.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShopSolution.WebApp.Controllers
+{
+    public class KhachHang : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ eShopSolution.WebApp/Controllers/Sach.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShopSolution.WebApp.Controllers
+{
+    public class Sach : Controller
+    {
+        public IActionResult Chitiet()
+        {
+            return View();
+        }
+    }
+}
